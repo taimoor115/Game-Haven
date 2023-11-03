7971d06ca3f5353d744d11f8389306cfd4b88535
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-11-03 22:59:12 +0500

    Build Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 const App = () => {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="red">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="brown">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main  " bg="yellow">
+          Main
+        </GridItem>
+      </Grid>
+    </>
+  );
 };
 
 export default App;
