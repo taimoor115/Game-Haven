ab69bae9019d6f1ffdacf1c9998ff1f45b3f594a
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-11-04 23:54:37 +0500

    Displaying  platforms icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
 import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
+
+export interface Platform {
+  id: number;
+  slug: string;
+  name: string;
+}
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
