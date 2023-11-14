2e727f4ca9532124f11ce785842ffedfbd413067
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-11-15 02:06:04 +0500

    Minor bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -16,49 +16,57 @@ export interface GameQuery {
   searchText: string;
 }
 
-const App = () => {
+function App() {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
+
   return (
-    <>
-      <Grid
-        templateAreas={{
-          base: `"nav" "main"`,
-          lg: `"nav nav" "aside main"`,
-        }}
-        templateColumns={{
-          base: "1fr",
-          lg: "230px 1fr",
-        }}
-      >
-        <GridItem area="nav">
-          <NavBar onSearch={(searchText) =>
-              setGameQuery({ ...gameQuery, searchText })
-            }
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "250px 1fr",
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar
+          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+        />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" paddingX={5}>
+          <GenreList
+            selectedGenre={gameQuery.genre}
+            onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
           />
         </GridItem>
-        <Show above="lg">
-          <GridItem area="aside" paddingX={7}>
-            <GenreList selectedGenre={gameQuery.genre} onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+      </Show>
+      <GridItem area="main">
+        <Box paddingLeft={2}>
+          <GameHeading gameQuery={gameQuery} />
+          <Flex marginBottom={5}>
+            <Box marginRight={5}>
+              <PlatformSelector
+                selectedPlatform={gameQuery.platform}
+                onSelectPlatform={(platform) =>
+                  setGameQuery({ ...gameQuery, platform })
+                }
+              />
+            </Box>
+            <SortSelector
+              sortOrder={gameQuery.sortOrder}
+              onSelectSortOrder={(sortOrder) =>
+                setGameQuery({ ...gameQuery, sortOrder })
+              }
             />
-          </GridItem>
-        </Show>
-        <GridItem area="main">
-          <Box paddingLeft={2}>
-            <GameHeading gameQuery={gameQuery} />
-            <Flex  marginBottom={5}>
-              <Box marginRight={5}>
-                <PlatformSelector  selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) =>
-                  setGameQuery({ ...gameQuery, platform })} />
-              </Box>
-              <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) =>
-                  setGameQuery({ ...gameQuery, sortOrder })} />
-            </Flex>
-          </Box>
-          <GameGrid gameQuery={gameQuery} />
-        </GridItem>
-      </Grid>
-    </>
+          </Flex>
+        </Box>
+        <GameGrid gameQuery={gameQuery} />
+      </GridItem>
+    </Grid>
   );
-};
+}
 
 export default App;
--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -9,11 +9,13 @@ import { GameQuery } from "../App";
 interface Props {
   gameQuery: GameQuery;
 }
+
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  if (error) return error && <Text>{error}</Text>;
+  if (error) return <Text>{error}</Text>;
+
   return (
     <SimpleGrid
       columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
@@ -23,12 +25,10 @@ const GameGrid = ({ gameQuery }: Props) => {
       {isLoading &&
         skeletons.map((skeleton) => (
           <GameCardContainer key={skeleton}>
-            {" "}
-            <GameCardSkeleton />{" "}
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
       {data.map((game) => (
-        // <li key={game.id}>{game.name}</li>
         <GameCardContainer key={game.id}>
           <GameCard game={game} />
         </GameCardContainer>
