b903904f04bcb1c63f6d545cb9bcc509a7c0c817
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-01-18 22:28:24 +0500

    Refractor: create lookups hooks

--- src/services/api-client.tsx
+++ src/services/api-client.tsx
@@ -3,7 +3,7 @@ import axios, { AxiosRequestConfig } from "axios";
 export interface FetchResponse<T> {
   count: number;
   results: T[];
-  next: string | undefined;
+  next: string | null;
 }
 
 // export default axios.create({
